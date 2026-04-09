6a402e7fbf122e6882a63e27e3bf074a07405308
Rudian420 <ahmedrudian@gmail.com>
2026-04-09 14:59:38 +0600

    c

--- backend/Database/config.php
+++ backend/Database/config.php
@@ -1,6 +1,6 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'u106340611_bucuc_2026');
-define('DB_USER', 'u106340611_bucuc');
+define('DB_NAME', 'u106340611_bucuc');
+define('DB_USER', 'u106340611_bucuc_2026');
 define('DB_PASS', 'Noob-22299005');
